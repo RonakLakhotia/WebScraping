dd9ebc1eb030cd3191d7c7be78f6d0d4ec71c992
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-12-22 12:40:39 +0800

    Debug

--- NBA/NbaPlayers.py
+++ NBA/NbaPlayers.py
@@ -1,13 +1,29 @@
-from selenium import webDriver
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class Player():
 
 	#attributes for each player
-	self.name = ""
-	self.link = ""
-	self.Height = ""
-	self.Weight = ""
-	self.DateOfBirth = ""
+	  name = ""
+	  link = ""
+	  Height = ""
+	  Weight = ""
+	  DateOfBirth = ""
+
+
+def getPlayerList():
+
+	driver = webdriver.PhantomJS(executable_path = '/Users/ronaklakhotia/Desktop/phantomjs')
+	url = 'https://stats.nba.com/players/list/?ls=iref:nba:gnav'
+
+	#download HTML page
+	driver.get(url)
+
+	#create soup
+	soup = BeautifulSoup(driver.page_source, 'lxml')
+	div = soup.find('div', class_ = 'columns / small-12 / section-view-overlay')
+
+
+getPlayerList()	
 
 	
